b07a0bc050dec66873be1026b5a0baa058c7c175
Kelsey Beyer <kmok@thoughtworks.com>
2023-02-03 11:53:28 +0100

    adding test_convert_stop_transaction_unit

--- src/exercise_ev_databricks_unit_tests/final_charge_time_charge_dispense_completed_charges.py
+++ src/exercise_ev_databricks_unit_tests/final_charge_time_charge_dispense_completed_charges.py
@@ -1,6 +1,9 @@
 from typing import Callable
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, ArrayType
 import pandas as pd
+import json
+from pyspark.sql.functions import col, from_json
+from pyspark.sql import DataFrame
 
 
 def test_return_stop_transaction_unit(spark, f: Callable):
@@ -35,5 +38,71 @@ def test_return_stop_transaction_unit(spark, f: Callable):
     result_actions = [x.action for x in result.collect()]
     expected_actions = ["StopTransaction"]
     assert result_actions == expected_actions, f"expect {expected_actions}, but got {result_actions}"
+
+    print("All tests pass! :)")
+
+
+def test_convert_stop_transaction_unit(spark, f: Callable):
+    input_pandas = pd.DataFrame([
+        {
+            "foo": "30e2ed0c-dd61-4fc1-bcb8-f0a8a0f87c0a",
+            "body": json.dumps({
+                "meter_stop": 26795,
+                "timestamp": "2022-10-02T15:56:17.000345+00:00",
+                "transaction_id": 1,
+                "reason": None,
+                "id_tag": "14902753768387952483",
+                "transaction_data": None
+            })
+        },
+    ])
+
+    input_df = spark.createDataFrame(
+        input_pandas,
+        StructType([
+            StructField("foo", StringType()),
+            StructField("body", StringType()),
+        ])
+    )
+
+    result = input_df.transform(f)
+
+    print("Transformed DF:")
+    result.show()
+
+    result_count = result.count()
+    assert result_count == 1
+
+    def get_json_value(df: DataFrame, column: str, key: str):
+        return [getattr(x, key) for x in df.select(col(f"{column}.{key}")).collect()][0]
+
+    assert get_json_value(result, "new_body",
+                          "meter_stop") == 26795, f"expected 26795, but got {get_json_value(result, 'new_body', 'meter_stop')}"
+    assert get_json_value(result, "new_body",
+                          "timestamp") == "2022-10-02T15:56:17.000345+00:00", f"expected '2022-10-02T15:56:17.000345+00:00', but got {get_json_value(result, 'new_body', 'timestamp')}"
+    assert get_json_value(result, "new_body",
+                          "transaction_id") == 1, f"expected 1, but got {get_json_value(result, 'new_body', 'transaction_id')}"
+    assert get_json_value(result, "new_body",
+                          "reason") == None, f"expected None, but got {get_json_value(result, 'new_body', 'reason')}"
+    assert get_json_value(result, "new_body",
+                          "id_tag") == "14902753768387952483", f"expected '14902753768387952483', but got {get_json_value(result, 'new_body', 'id_tag')}"
+    assert get_json_value(result, "new_body",
+                          "transaction_data") == None, f"expected None, but got {get_json_value(result, 'new_body', 'transaction_data')}"
+
+    result_schema = result.schema
+    expected_schema = StructType([
+        StructField('foo', StringType(), True),
+        StructField('body', StringType(), True),
+        StructField('new_body',
+                    StructType([
+                        StructField('meter_stop', IntegerType(), True),
+                        StructField('timestamp', StringType(), True),
+                        StructField('transaction_id', IntegerType(), True),
+                        StructField('reason', StringType(), True),
+                        StructField('id_tag', StringType(), True),
+                        StructField('transaction_data', ArrayType(StringType(), True), True)]),
+                    True)
+    ])
+    assert result_schema == expected_schema, f"expected {expected_schema}, but got {result_schema}"
 
     print("All tests pass! :)")
