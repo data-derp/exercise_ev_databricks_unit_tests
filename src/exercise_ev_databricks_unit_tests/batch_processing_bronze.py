bc73ce1905ee6b0e5bf05b4066ffbcd84c7ba602
Kelsey Beyer <kelseymok@gmail.com>
2023-04-11 22:53:41 +0200

    adding write e2e

--- src/exercise_ev_databricks_unit_tests/batch_processing_bronze.py
+++ src/exercise_ev_databricks_unit_tests/batch_processing_bronze.py
@@ -124,3 +124,13 @@ def test_write_dd_e2e(input_df: DataFrame, spark, display_f, **kwargs):
     assert result_name == expected_name, f"Expected {expected_name}, but got {result_name}"
 
     print("All tests pass! :)")
+
+def test_write_e2e(input_df: DataFrame, spark, display_f, **kwargs):
+    result = spark.createDataFrame(input_df)
+    display_f(result)
+    result_count = result.filter(result.name.endswith(".snappy.parquet")).count()
+
+    expected_count = 1
+    assert result_count == expected_count, f"Expected {expected_count}, but got {result_count}"
+
+    print("All tests pass! :)")
